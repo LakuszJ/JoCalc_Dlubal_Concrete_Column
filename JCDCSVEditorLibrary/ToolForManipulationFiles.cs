4fb6029847089a754a737053e316fc975e5ae4b7
LakuszJ <l.jozefczyk@gmail.com>
2022-08-28 20:08:39 +0200

    First release - v.1.0.1

--- JCDCSVEditorLibrary/ToolForManipulationFiles.cs
+++ JCDCSVEditorLibrary/ToolForManipulationFiles.cs
@@ -26,27 +26,13 @@ namespace JCDlubalCSVForcesGeneratorLibrary
             }
         }
 
-
-        //static public object DeserializeJson(/*string jsonPath)*/)
-        //{
-        //    //string testString =
-        //    //    "{\"readTime\":\"0001-01-01T00:00:00\"," +
-        //    //    "\"CSVFilePath\":\"C:\\\\Users\\\\ljoze\\\\source\\\\repos\\\\JoCalcDlubalClient\\\\JoCalcDlubalClientConsoleUI\\\\bin\\\\Debug\\\\net6.0\\\\CSV_Project_Data\\\\Model_1\"," +
-        //    //    "\"newCombinationsCSVFileNames\":{\"CO1\":\"CO1_static_analysis_members_internal_forces.csv\",\"CO2\":\"CO2_static_analysis_members_internal_forces.csv\",\"CO3\":\"CO3_static_analysis_members_internal_forces.csv\",\"CO4\":\"CO4_static_analysis_members_internal_forces.csv\",\"CO5\":\"CO5_static_analysis_members_internal_forces.csv\",\"CO6\":\"CO6_static_analysis_members_internal_forces.csv\",\"CO7\":\"CO7_static_analysis_members_internal_forces.csv\"}," +
-        //    //    "\"appicationRFEMCuluture\":\"en-GB\"}"
-        //    //    ;
-        //    //object stObj = JsonConvert.DeserializeObject<>(testString); // change for jsonPath
-        //    //return stObj;
-        //}
-
         public static void SaveTxtFile(string path, string source)
         {
-            File.WriteAllText(path, source);
-            //using (StreamWriter sw = new StreamWriter(path))
-            //{
-            //    sw.Write(source);
-            //    sw.Close();
-            //}
+            using (StreamWriter sw = new StreamWriter(path))
+            {
+                sw.Write(source);
+                sw.Close();
+            }
         }
     }
 
